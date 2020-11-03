9094732546996cea8765ccee61c6a3ffb6cd7ebe
huytung255 <61749223+huytung255@users.noreply.github.com>
2020-11-03 22:49:18 +0700

    Update app.js

--- app.js
+++ app.js
@@ -37,5 +37,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => console.log(`Server ${PORT}`));
 module.exports = app;
